d3115c4b797a6827e6c06cfa5f53664d3473cd81
kbws <hsy040506@163.com>
2023-11-11 16:57:21 +0800

    给代码沙箱提供开发API

--- src/main/java/xyz/kbws/ojcodesandbox/controller/MainController.java
+++ src/main/java/xyz/kbws/ojcodesandbox/controller/MainController.java
@@ -1,7 +1,14 @@
 package xyz.kbws.ojcodesandbox.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.kbws.ojcodesandbox.JavaNativeCodeSandbox;
+import xyz.kbws.ojcodesandbox.model.ExecuteCodeRequest;
+import xyz.kbws.ojcodesandbox.model.ExecuteCodeResponse;
+
+import javax.annotation.Resource;
 
 /**
  * @author kbws
@@ -11,8 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 public class MainController {
 
+    @Resource
+    private JavaNativeCodeSandbox javaNativeCodeSandbox;
+
     @GetMapping("/health")
     public String healthCheck(){
         return "ok";
+    }
+
+    /**
+     * 执行代码
+     * @param executeCodeRequest
+     * @return
+     */
+    @PostMapping("/executeCode")
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest) {
+        if (executeCodeRequest == null) {
+            throw new RuntimeException("请求参数为空");
+        }
+        return javaNativeCodeSandbox.executeCode(executeCodeRequest);
     }
 }
