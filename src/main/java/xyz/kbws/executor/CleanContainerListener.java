bfff552e1366de62f7d09dc50cc5628eed6abe46
kbws13 <hsy040506@163.com>
2024-11-02 15:24:40 +0800

    feat(executor): 添加容器清理监听器
    - 实现了 CleanContainerListener 类，监听应用关闭事件- 在应用关闭时，清理所有容器及其相关文件
    - 优化了资源清理逻辑，确保应用优雅退出

---
+++ src/main/java/xyz/kbws/executor/CleanContainerListener.java
@@ -0,0 +1,37 @@
+package xyz.kbws.executor;
+
+import cn.hutool.core.io.FileUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author kbws
+ * @date 2024/11/2
+ * @description:
+ */
+@Slf4j
+@Component
+public class CleanContainerListener implements ApplicationListener<ContextClosedEvent> {
+
+    @Resource
+    private DockerDao dockerDao;
+
+    @Resource
+    private ContainerPoolExecutor containerPoolExecutor;
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent event) {
+        // 清理所有容器以及残余文件
+        containerPoolExecutor
+                .getContainerPool()
+                .forEach(containerInfo -> {
+                    FileUtil.del(containerInfo.getCodePathName());
+                    dockerDao.cleanContainer(containerInfo.getContainerId());
+                });
+        log.info("container clean end...");
+    }
+}
